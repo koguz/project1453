92dfd21d7c0c2665b8e3f15a83a3f81931afbaa8
kayaoguz <kayaoguz@54d33ca4-fc1f-0410-8956-7bd2e8604934>
2007-03-07 00:42:08 +0000

    temizlik

--- trunk/gui/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Application.h"
-
-using namespace std;
-
-int main(int argc, char* argv[])
-{
-	Application *app = new Application("SDLGui Testing", 640, 480, 16, SDL_SWSURFACE);
-	return app->Run();
-}
